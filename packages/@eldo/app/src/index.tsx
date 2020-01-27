909f2e48748cd9a402631f8c3a519b7892ef0b29
Jack Tan <jacktan165@gmail.com>
2020-01-27 19:31:51 +1300

    WIP; Added the initial layout for a lerna project with verdaccio registry

--- packages/@eldo/app/src/index.tsx
+++ packages/@eldo/app/src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <div>Test</div>;
+import Button from "@eldo/ui/Button";
+
+const App = () => <Button text="test">Test</Button>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/components/Button/Button.stories.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-storiesOf("Test", module).add("with an image", () => <div>test</div>);
