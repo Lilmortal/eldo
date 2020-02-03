83c5d58b7b5fead1336b206aac7566060f5f4dcf
Jack Tan <jacktan165@gmail.com>
2020-02-03 16:29:04 +0900

    feat: addded jest config package

---
+++ packages/@eldo/jest-config/jest.config.js
@@ -0,0 +1,29 @@
+const setupTests = (module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "clover"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  modulePathIgnorePatterns: ["dist"],
+  // Because Jest only knows how to read Javascript, we need to mock non Javascript files
+  // to a Javascript file.
+  moduleNameMapper: {
+    // "@eldo/(.+)$": "<rootDir>packages/$1",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+  notify: true,
+  notifyMode: "always",
+  roots: ["<rootDir>packages"],
+  testMatch: ["**/__tests__/*.+(ts|tsx|js)", "**/*.test.+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  // setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+});
