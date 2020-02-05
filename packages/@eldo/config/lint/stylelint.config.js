a7e14d792e11f04a576f532186e975ae362e3eba
Jack Tan <jacktan165@gmail.com>
2020-02-05 15:15:30 +0900

    feat: upgraded eslint tool dependencies on @eldo/styles

--- packages/@eldo/config/lint/stylelint.config.js
+++ packages/@eldo/config/lint/stylelint.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
-  rules: { 'declaration-block-trailing-semicolon': 'always' },
+  rules: {
+    'declaration-block-trailing-semicolon': 'always',
+    'selector-nested-pattern': '^&',
+  },
 };
