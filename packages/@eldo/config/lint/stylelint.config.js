9dc508458932df5465d1ec783a60d97ab908ddf6
Jack Tan <jacktan165@gmail.com>
2020-02-06 17:27:02 +0900

    fix: removed /recommended from stylelint-a11y

--- packages/@eldo/config/lint/stylelint.config.js
+++ packages/@eldo/config/lint/stylelint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     'stylelint-config-recommended',
     'stylelint-config-prettier',
-    'stylelint-a11y/recommended',
+    'stylelint-a11y',
   ],
   rules: {
     'declaration-block-trailing-semicolon': 'always',
